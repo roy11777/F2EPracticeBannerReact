7db944cc29bc11c83fa99a8150b9614360cbbb50
roy <roy>
2019-07-30 16:28:31 +0800

    New

--- reactbanner/src/App.js
+++ reactbanner/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div class="banner">
+      <div>
+        <a className="wrap" href="#">
+          <img
+            class="img"
+            src="./imgs/big.jpg"
+            title="輸入廣告促銷說明文字"
+            alt="輸入廣告促銷說明文字"
+          />
         </a>
-      </header>
+        <button></button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
